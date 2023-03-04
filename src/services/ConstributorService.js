29db8796cb1611d6fe39feae930da5780bfef9b2
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-03-04 14:20:28 +0700

    chore(fix): Octokit auth

--- src/services/ConstributorService.js
+++ src/services/ConstributorService.js
@@ -1,13 +1,20 @@
 import { Octokit } from "https://cdn.skypack.dev/@octokit/rest"
 
 export const getAllContributors = async () => {
-    const github = new Octokit({
-        auth: import.meta.env.VITE_GITHUB_TOKEN
-    })
+    try {
+        const github = new Octokit({
+            auth: import.meta.env.VITE_GITHUB_TOKEN
+        })
+        const owner = 'StreetCommunityProgrammer'
+        const repo = 'metaphore'
 
-    const contributors = await github.rest.repos.listContributors({
-        owner: 'StreetCommunityProgrammer',
-        repo: 'metaphore',
-    });
-    return contributors.data
+        const contributors = await github.rest.repos.listContributors({
+            owner,
+            repo
+        })
+        return contributors.data
+    } catch (error) {
+        console.log(`Error on getAllContributors: ${error}`)
+        return 0
+    }
 }
