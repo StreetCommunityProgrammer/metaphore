25b3cf0b119d87a34e1d12cc40def9d8711c8ecc
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-03-06 13:41:50 +0700

    chore(routes): adding group routes for each language pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router-dom"
 import { menu } from "./routes/menu"
 import BaseLayout from "./layouts/BaseLayout"
 import { sidebar } from "./routes/sidebar"
+import { routers } from "./routes/routers"
 
 function App() {
     return (
         <Routes>
             <Route element={<BaseLayout />}>
-                {[...menu, ...sidebar].map((item, index) => (<Route key={index} path={item.path} element={item.component} />))}
+                {[...menu, ...sidebar, ...routers].map((item, index) => (<Route key={index} path={item.path} element={item.component} />))}
             </Route>
         </Routes>
     )
