f25668b8cf28c9d0c21047e906966ce80e065e68
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-03-14 14:31:26 +0700

    chore(config): adding app and seo banner config

---
+++ src/constants/app-config.js
@@ -0,0 +1,8 @@
+export const NEXT_BASE_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:2312' : 'https://metaphore.vercel.app'
+export const NEXT_APP_NAME = 'Metaphore (SCP)'
+export const NEXT_APP_DESCRIPTION =
+	'The Gray Line of Internet. Story as Code. Public Collections of Metaphor our Freestylers accross the world. Gain knowledge with unusual perspective from our Punk members';
+export const NEXT_APP_AUTHOR = [
+    'https://github.com/darkterminal',
+    'https://github.com/mkubdev'
+]
