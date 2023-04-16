37fecd87da4542854169d2d47072bb9f464dd078
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-04-16 10:32:56 +0700

    feat(navbar): add z-index to dropdown menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ function Navbar() {
 					</label>
 					<ul
 						tabIndex={0}
-						className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+						className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 z-auto"
 					>
 						<li>
 							<ActiveLink href="/our-hustle-and-grind">
